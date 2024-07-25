861a2c209df0258923f9e951b4e6f0d45a38a667
Tvnshh <tvanmaccai@gmail.com>
2024-07-25 14:28:07 +0800

    test

--- BIBLIOMANIA/conn.php
+++ BIBLIOMANIA/conn.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
