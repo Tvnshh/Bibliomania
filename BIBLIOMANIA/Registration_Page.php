0fbf32b492258e85c0f72bdb2a2eae7bdfd10136
KieranGoh <kieranmatthewgoh@gmail.com>
2024-08-12 14:48:52 +0800

    styling registration page

--- BIBLIOMANIA/Registration_Page.php
+++ BIBLIOMANIA/Registration_Page.php
@@ -22,6 +22,125 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Sign Up</title> 
     <link rel="stylesheet" href="styles.css">
 </head>
+<style>
+    body {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    }
+    .image {
+        margin-right: 150px;
+        box-shadow: 5px 5px 50px rgba(0, 0, 0, 0.6);
+        border-radius: 20px;
+    }
+    .image img {
+        width: 40vw;
+    }
+    .container {
+        background: rgb(27,27,27);
+        box-shadow: 10px 10px 40px rgba(221, 83, 49, 0.5);
+        border-style: solid;
+        border-color: rgb(221, 83, 49);
+        border-radius: 1.5vw;
+        padding: 1.5vw;
+        padding-right: 3vw;
+        width: 29vw;
+        text-align: center;
+    }
+    .header {
+        margin-bottom: 1.5vw;
+        text-align: left;
+    }
+    h1 {
+        font-size: 3vw;
+        text-align: center;
+        margin-bottom: 0;
+        color: rgb(221, 83, 49);
+    }
+    .form-container {
+        margin-bottom: 1vw;
+    }
+    label {
+        display: block;
+        margin-top: 1.5vw;
+        color: rgb(221, 83, 49);
+        font-size: 1.3vw;
+        text-align: left;
+    }
+    input[type="name"],
+    input[type="password"],
+    input[type="email"],
+    input[type="age"],
+     {
+        width: 100%;
+        font-size: 1.1vw;
+        padding: 0.65vw;
+        box-shadow: 5px 5px 10px rgba(221, 83, 49, 0.5);
+        border: 1px solid #ccc;
+        border-radius: 0.5vw;
+        margin-top: 0.3vw;
+    }
+    input[type="submit"] {
+        background-color: rgb(221, 83, 49);
+        text-align: center;
+        font-size: 1.5vw;
+        font-family: 'CustomFont';
+        width: 11vw;
+        height: 3.5vw;
+        color: whitesmoke;
+        border: solid;
+        border-color: rgb(27,27,27);
+        border-radius: 0.5vw;
+        cursor: pointer;
+        transition: font-size 0.2s ease;
+        margin-top: 3vw;
+    }
+    input[type="submit"]:hover {
+        background-color: rgb(27,27,27);
+        border: solid;
+        border-color: rgb(221, 83, 49);
+        font-size: 1.8vw;
+    }
+    .register p {
+        margin-top: 3vw;
+        font-size: 1vw;
+        text-align: center;
+        color: rgb(221, 83, 49);
+    }
+    .register a {
+        color: #93c7ff;
+        text-decoration: none;
+    }
+    .container p {
+        font-size: 1.3vw;
+        color: red ;
+    }
+    .container button {
+        background-color: rgb(221, 83, 49);
+        text-align: center;
+        font-size: 1.5vw;
+        font-family: 'CustomFont';
+        width: 11vw;
+        height: 3.5vw;
+        color: whitesmoke;
+        border: solid;
+        border-color: rgb(27,27,27);
+        border-radius: 0.5vw;
+        cursor: pointer;
+        transition: font-size 0.2s ease;
+        margin-top: 3vw;
+        margin: auto;
+    }
+    .container button:hover {
+        background-color: rgb(27,27,27);
+        border: solid;
+        border-color: rgb(221, 83, 49);
+        font-size: 1.8vw;
+    }
+    </style>
 <body>
     <form action="Registration_Page.php" method="post">
         <h1>Sign Up</h1>
